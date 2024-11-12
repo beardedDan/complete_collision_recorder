9ac1b852e7f51dc720a83027c587266121941e98
Daniel Zielinski <dzielinski06@runbox.com>
2024-11-12 12:08:05 -0500

    Added Gemini model scoring to scoring runtime module.

--- bin/score_and_describe_reports.py
+++ bin/score_and_describe_reports.py
@@ -4,6 +4,7 @@ import sys
 import os
 import numpy as np
 import pickle
+import argparse
 
 
 # Append the src directory to sys.path and import the src modules
@@ -18,6 +19,10 @@ import utils as u
 # Map the project directories
 root_dir, src_dir, data_dir, models_dir = u.map_project_directories()
 
+# Set up argument parsing and define the passed arguments
+parser = argparse.ArgumentParser(description="Run project script with optional testing flag.")
+parser.add_argument('--testing', action='store_true', help='Reduce dataframe size for testing')
+args = parser.parse_args()
 
 # Clean descriptions from the reports
 training_df = pd.read_csv(os.path.join(data_dir, "processed", "training_df.csv"))
@@ -50,3 +55,17 @@ GCAT_pred_df['concatenated_text'] = GCAT_pred_df['concatenated_text'] + '\n\n GC
 output_path = os.path.join(data_dir, "processed", "GCAT_pred_text.csv")
 GCAT_pred_df[['concatenated_text', 'BIKE_CLE_TEXT']].to_csv(output_path, index=False)
 print(f"Saved GCAT predictions to {output_path}")
+
+# Reduce the size of the dataframe for testing
+if args.testing:
+    print("Running in testing mode. Only the first 5 rows will be scored by GeminiAI model.")
+    GCAT_pred_df = GCAT_pred_df[:5] # For testing
+else:
+    print("Running in normal mode. Full dataset will be scored by GeminiAI model. This may take a while.")
+
+# Create Narratives of Collisions using GeminiBikeCleModel
+GeminiBikeCleModel = cc.GenBikeCleNarrative(google_api_key="CCR_API")
+GCAT_pred_df['GenAISummary'] = GCAT_pred_df['concatenated_text'].apply(lambda x: GeminiBikeCleModel.summarize(x))
+
+# bikeCLE_input_df
+GCAT_pred_df.to_csv(os.path.join(data_dir, "processed", "GenAI_df.csv"))
